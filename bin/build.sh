ac79390bd9a60b0d002df7593ac3221ca72c7ac8
Tom <eenzor@gmail.com>
2020-09-10 18:20:28 +1000

    set error flags in bash scripts

--- bin/build.sh
+++ bin/build.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+set -euo pipefail
+
 COMMIT=$(git rev-parse --short HEAD)
 VERSION=$(grep -E "## \[[0-9]*.[0-9]*.[0-9]*\]" CHANGELOG.md | head -1 | cut -d "[" -f2 | cut -d "]" -f1 )
 
